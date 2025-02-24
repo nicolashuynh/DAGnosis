dfafa20ac78ec3fd8f1d8cf34996395f719ccab9
nicolas <nicolas.huynh99@gmail.com>
2025-02-25 00:05:44 +0100

    Initial commit

--- scripts/debug_linear.sh
+++
@@ -1,3 +0,0 @@
-python ../experiments/synthetic/generate_data.py sem_type=linear name_experiment=debug n_repetitions=1
-python ../experiments/synthetic/train_cp.py name_experiment=debug n_repetitions=1
-python ../experiments/synthetic/test_cp.py name_experiment=debug n_repetitions=1
